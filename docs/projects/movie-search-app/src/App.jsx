8b90c753bf0b39a8203610ceb74fb372095eeda8
arminshami7 <www.arminshami7@gmail.com>
2025-12-06 05:44:20 +0330

    feat: complete movie search app functionality and styling

--- docs/projects/movie-search-app/src/App.jsx
+++ docs/projects/movie-search-app/src/App.jsx
@@ -2,6 +2,8 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import './App.css';
+import MovieDetailPage from './pages/MovieDetailPage';
+import TestPage from './pages/TestPage';
 
 function App(){
   return(
@@ -9,7 +11,9 @@ function App(){
 
       <Routes>
 
-        <Route path="/" element={<HomePage />} />
+        <Route path = "/" element={<HomePage />} />
+        <Route path = "/movie/:id" element = {<MovieDetailPage />} />
+        <Route path ="/test" element={<TestPage />} />
 
       </Routes>
     </div>
--- docs/projects/movie-search-app/src/components/MovieCard.jsx
+++ docs/projects/movie-search-app/src/components/MovieCard.jsx
@@ -1,14 +1,14 @@
 // src/components/MovieCard.jsx
-import {link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 function MovieCard({ movie }) { 
     return (
-        <link to={`/movie/${movie.imdbID}`}>
+        <Link to={`/movie/${movie.imdbID}`}>
         <div className="movie-card">
             <img src={movie.Poster} alt={movie.Title}/>
             <h3>{movie.Title}</h3>
             <p>{movie.Year}</p>
         </div>
-        </link>
+        </Link>
     );
 }
 export default MovieCard;
